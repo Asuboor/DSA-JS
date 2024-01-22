ae13bbddd5e1718fbe507b39d895c1288fa8d945
Abdul Suboor <106346130+Asuboor@users.noreply.github.com>
2024-01-22 16:14:30 +0530

    updated

---
+++ Arrays/array-test.js
@@ -0,0 +1,28 @@
+// const arr1=[1,2,3]
+// const arr2=[4,5,6]
+
+//spread operator
+// const finalArr=[...arr1,...arr2]
+
+//concatination
+// const finalArr=arr1.concat(arr2)
+
+//splice: (include,reject)
+// const finalArr=arr1.slice(1,2)
+
+//splice
+// const finalArr=arr2.splice(1,2)
+
+//findIndex
+// const finalArr=arr1.findIndex((item)=> item===1)
+
+//reverse
+// const finalArr=arr2.reverse()
+
+// console.log(finalArr);
+
+const arr = [60,210,2];
+
+arr.sort((a,b)=>a-b)
+if(arr)
+console.log(arr[1]);
